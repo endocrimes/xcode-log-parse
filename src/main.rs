707e6bffdb37868b597cf365742723ee818c3470
Samuel Giddins <segiddins@squareup.com>
2018-09-16 12:31:52 +0200

    Start parser

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io::{self, BufRead};
 pub struct ParserResult {}
 pub struct ParserError {}
 
-pub trait Parser {
+pub trait ParserTrait {
     fn read_line(line: &str) -> Result<ParserResult, ParserError>;
 }
 
@@ -17,6 +17,7 @@ fn main() {
     let matches = App::new("xcode-log-parse")
         .version("0.1.0")
         .about("Parse, filter, and output Xcode build logs.")
+        .arg(Arg::with_name("do-foo").long("do-foo").help(""))
         .arg(Arg::with_name("formatter").default_value("plain").help(""))
         .get_matches();
 
@@ -25,6 +26,11 @@ fn main() {
         Ok(f) => f,
         Err(e) => panic!("Error: {}: {}", e, formatter_name)
     };
+
+    match matches.occurrences_of("do-foo") {
+        0 => {},
+        _ => { do_foo(); return },
+    }
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
@@ -50,5 +56,49 @@ impl PlainTextFormatter {
 
 impl Formatter for PlainTextFormatter {
     fn format(&mut self, _r: ParserResult) {
+    }
+}
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[cfg(debug_assertions)]
+const _GRAMMAR: &'static str = include_str!("xcodebuild.pest"); // relative to this file
+
+#[derive(Parser)]
+#[grammar = "xcodebuild.pest"]
+struct XcodebuildParser;
+
+fn do_foo() {
+    let input = "CopyStringsFile /Users/segiddins/Development/Square/xcodebuildprofiler/build/Release/Xcode\\ Build\\ Profiler.app/Contents/Resources/en.lproj/InfoPlist.strings /Users/segiddins/Development/Square/xcodebuildprofiler/XcodeBuildProfiler/en.lproj/InfoPlist.strings (in target: Xcode Build Profiler)
+    cd /Users/segiddins/Development/Square/xcodebuildprofiler
+    builtin-copyStrings --validate --outputencoding UTF-16 --outdir /Users/segiddins/Development/Square/xcodebuildprofiler/build/Release/Xcode\\ Build\\ Profiler.app/Contents/Resources/en.lproj -- /Users/segiddins/Development/Square/xcodebuildprofiler/XcodeBuildProfiler/en.lproj/InfoPlist.strings
+note: detected encoding of input file as Unicode (UTF-8)";
+    println!("{}", input);
+
+    let pairs = XcodebuildParser::parse(Rule::entire, input).unwrap_or_else(|e| panic!("{}", e));
+
+    // Because ident_list is silent, the iterator will contain idents
+    for full_command in pairs {
+        match full_command.as_rule() {
+            Rule::full_command => println!("full command"),
+            _ => unreachable!()
+        }
+
+        for pair in full_command.into_inner() {
+            
+            // A pair is a combination of the rule which matched and a span of input
+            println!("Rule:    {:?}", pair.as_rule());
+            println!("Span:    {:?}", pair.clone().into_span());
+            println!("Text:    {:?}", pair.clone().into_span().as_str());
+
+            // A pair can be converted to an iterator of the tokens which make it up:
+            for inner_pair in pair.into_inner() {
+                println!("\t{:?}: {}", inner_pair.as_rule(), inner_pair.into_span().as_str());
+            }
+        }
     }
 }
